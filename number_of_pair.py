9077d01e122267e5708b12293c8cb6d9801cdc46
Jenil Desai <jenil.desai25@gmail.com>
2018-11-04 18:56:00 -0600

    added 2nd approach

--- number_of_pair.py
+++ number_of_pair.py
@@ -34,3 +34,43 @@ def number_of_pair(a, k):
 a = [1, 1, 3, 3, 9, 46]
 k = 47
 print(number_of_pair(a, k))
+
+
+def numberOfPairs(a, k):
+    """
+    Runtime: O(n * log n)
+    After sorting, I can iterate through the
+    list from both sides in O(n) to find the pairs.
+    """
+    a.sort()
+    if len(a) < 2:
+        return []
+    pairs = []
+    start = 0
+    end = len(a) - 1
+    while start < end:
+        n1 = arr[start]
+        n2 = arr[end]
+        pair = n1 + n2
+        if pair < k:
+            start += 1
+        if pair > k:
+            end -= 1
+        if pair == k:
+            pairs.append([n1, n2])
+            start += 1
+    container = {}
+    for i in pairs:
+        if i[1] not in container:
+            key = i[0]
+            value = i[1]
+            container[key] = value
+    if len(container) > 0:
+        return len(container)
+    else:
+        return 0
+
+
+k = 12
+arr = [6, 6, 3, 9, 3, 5, 1]
+print(numberOfPairs(arr, k))
